f673840abb661b2856500b63d146c8ed10c5f529
Netboy3 <1472804+netboy3@users.noreply.github.com>
2022-01-14 21:33:47 -0500

    uQuant: Fix displays to show in browser and cleanup
    * Major cleanup of the module including removing
      part of the code that replicated section of the
      module's process eight times.
    * Bringing over the single widget from UI.hpp,
      deleteing UI.cpp and cleaning up a large portion
      of UI.hpp
    * Labeling I/O (V2 feature) as the panel has no labels

--- src/uQuant.cpp
+++ src/uQuant.cpp
@@ -14,100 +14,74 @@ https://github.com/jhoar/AmalgamatedHarmonics
 #include <sstream>
 #include <iomanip>
 
-// Displays
-struct uQuantDisplayWidget : TransparentWidget {
-
-  std::string *value;
-
-  void draw(NVGcontext *vg) override
-  {
-    // Background
-    NVGcolor backgroundColor = nvgRGB(0xC0, 0xC0, 0xC0);
-    nvgBeginPath(vg);
-    nvgRoundedRect(vg, 0.0, 0.0, box.size.x, box.size.y, 4.0);
-    nvgFillColor(vg, backgroundColor);
-    nvgFill(vg);
-
-    // text
-    std::shared_ptr<Font> font = APP->window->loadFont(asset::plugin(pluginInstance, "res/Pokemon.ttf"));
-    if (font) {
-    nvgFontSize(vg, 15);
-    nvgFontFaceId(vg, font->handle);
-    nvgTextLetterSpacing(vg, 2);
-    }
-
-    std::stringstream to_display;
-    to_display << std::setw(3) << *value;
-
-    Vec textPos = Vec(2.0f, 17.0f);
-    NVGcolor textColor = nvgRGB(0x00, 0x00, 0x00);
-    nvgFillColor(vg, textColor);
-    nvgText(vg, textPos.x, textPos.y, to_display.str().c_str(), NULL);
-  }
-};
-
 // Module
-struct uQuant : AHModule {
+struct uQuant : Module {
 
     enum ParamIds {
         KEY_PARAM,
         SCALE_PARAM,
-        ENUMS(SHIFT_PARAM,8),
+        SHIFT_PARAM,
         TRANS_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
-        ENUMS(IN_INPUT,8),
+        IN_INPUT,
         KEY_INPUT,
         SCALE_INPUT,
         TRANS_INPUT,
-        ENUMS(HOLD_INPUT,8),
+        HOLD_INPUT,
         NUM_INPUTS
     };
     enum OutputIds {
-        ENUMS(OUT_OUTPUT,8),
-        ENUMS(TRIG_OUTPUT,8),
+        OUT_OUTPUT,
+        TRIG_OUTPUT,
         NUM_OUTPUTS
     };
     enum LightIds {
-        ENUMS(KEY_LIGHT,12),
-        ENUMS(SCALE_LIGHT,12),
         NUM_LIGHTS
     };
 
-    uQuant() : AHModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
-    configParam(uQuant::KEY_PARAM, 0.0f, 11.0f, 0.0f, "");
-    configParam(uQuant::SCALE_PARAM, 0.0f, 11.0f, 0.0f, "");
-    configParam(uQuant::SHIFT_PARAM, -3.0f, 3.0f, 0.0f, "");
-    configParam(uQuant::TRANS_PARAM, -11.0f, 11.0f, 0.0f, "");
+    uQuant() {
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        configParam(KEY_PARAM, 0.0f, 11.0f, 0.0f, "Key");
+        configParam(SCALE_PARAM, 0.0f, 11.0f, 0.0f, "Scale");
+        configParam(SHIFT_PARAM, -3.0f, 3.0f, 0.0f, "Shift");
+        configParam(TRANS_PARAM, -11.0f, 11.0f, 0.0f, "Transpose");
+        configInput(IN_INPUT, "Main");
+        configInput(KEY_INPUT, "Key CV");
+        configInput(SCALE_INPUT, "Scale CV");
+        configInput(TRANS_INPUT, "Transpose CV");
+        configInput(HOLD_INPUT, "Hold");
+        configOutput(OUT_OUTPUT, "Main");
+        configOutput(TRIG_OUTPUT, "Trigger");
     }
 
-    void step() override;
+    void process(const ProcessArgs& args) override;
 
     bool firstStep = true;
     int lastScale = 0;
     int lastRoot = 0;
     float lastTrans = -10000.0f;
 
-    dsp::SchmittTrigger holdTrigger[8];
-    float holdPitch[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0};
-    float lastPitch[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0};
-    AHPulseGenerator triggerPulse[8];
+    dsp::SchmittTrigger holdTrigger;
+    float holdPitch = 0.0f;
+    float lastPitch = 0.0f;
+    AHPulseGenerator triggerPulse;
 
     std::string keys[12] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
     std::string scales[12] = {"Ch", "Io", "Do", "Ph", "Ly", "Mi", "Ae", "Lo", "5M", "5m", "Hm", "Bl"};
 
-    std::string keyValue[1] = {"C"};
-    std::string scaleValue[1] = {"C"};
+    std::map<std::string, std::string> display_data {
+        {"key_value", "C"},
+        {"scale_value", "C"}
+    };
 
     int currScale = 0;
     int currRoot = 0;
 
 };
 
-void uQuant::step() {
-
-    AHModule::step();
+void uQuant::process(const ProcessArgs& args) {
 
     lastScale = currScale;
     lastRoot = currRoot;
@@ -141,54 +115,92 @@ void uQuant::step() {
         }
     }
 
-    for (int i = 0; i < 8; i++) {
-        float holdInput     = inputs[HOLD_INPUT + i].value;
-        bool  holdActive    = inputs[HOLD_INPUT + i].active;
-        bool  holdStatus    = holdTrigger[i].process(holdInput);
+    float holdInput = inputs[HOLD_INPUT].value;
+    bool holdActive = inputs[HOLD_INPUT].active;
+    bool holdStatus = holdTrigger.process(holdInput);
 
-        float volts = inputs[IN_INPUT + i].value;
-        float shift = params[SHIFT_PARAM + i].value;
+    float volts = inputs[IN_INPUT].value;
+    float shift = params[SHIFT_PARAM].value;
 
-        if (holdActive) {
-
-            // Sample the pitch
-            if (holdStatus && inputs[IN_INPUT + i].active) {
-                holdPitch[i] = CoreUtil().getPitchFromVolts(volts, currRoot, currScale, &currNote, &currDegree);
-            }
-
-        } else {
-
-            if (inputs[IN_INPUT + i].active) {
-                holdPitch[i] = CoreUtil().getPitchFromVolts(volts, currRoot, currScale, &currNote, &currDegree);
-            }
-
+    if (holdActive) {
+        // Sample the pitch
+        if (holdStatus && inputs[IN_INPUT].active) {
+            holdPitch = CoreUtil().getPitchFromVolts(volts, currRoot, currScale, &currNote, &currDegree);
         }
 
-        // If the quantised pitch has changed
-        if (lastPitch[i] != holdPitch[i]) {
-            // Pulse the gate
-            triggerPulse[i].trigger(Core::TRIGGER);
-
-            // Record the pitch
-            lastPitch[i] = holdPitch[i];
+    } else {
+        if (inputs[IN_INPUT].active) {
+            holdPitch = CoreUtil().getPitchFromVolts(volts, currRoot, currScale, &currNote, &currDegree);
         }
-
-        if (triggerPulse[i].process(delta)) {
-            outputs[TRIG_OUTPUT + i].value = 10.0f;
-        } else {
-            outputs[TRIG_OUTPUT + i].value = 0.0f;
-        }
-
-        outputs[OUT_OUTPUT + i].value = holdPitch[i] + shift + trans;
-
     }
 
-    keyValue[0] = std::string(keys[currRoot]);
-    scaleValue[0] = std::string(scales[currScale]);
+    // If the quantised pitch has changed
+    if (lastPitch != holdPitch) {
+        // Pulse the gate
+        triggerPulse.trigger(Core::TRIGGER);
+
+        // Record the pitch
+        lastPitch = holdPitch;
+    }
+
+    if (triggerPulse.process(args.sampleTime)) {
+        outputs[TRIG_OUTPUT].value = 10.0f;
+    } else {
+        outputs[TRIG_OUTPUT].value = 0.0f;
+    }
+
+    outputs[OUT_OUTPUT].value = holdPitch + shift + trans;
+
+    display_data["key_value"] = std::string(keys[currRoot]);
+    display_data["scale_value"] = std::string(scales[currScale]);
 
     firstStep = false;
 
 }
+
+// Displays
+struct uQuantDisplayWidget : TransparentWidget {
+
+  uQuant *module;
+  std::string val_type;
+
+  void draw(NVGcontext *vg) override
+  {
+    // Background
+    NVGcolor backgroundColor = nvgRGB(0xC0, 0xC0, 0xC0);
+    nvgBeginPath(vg);
+    nvgRoundedRect(vg, 0.0, 0.0, box.size.x, box.size.y, 4.0);
+    nvgFillColor(vg, backgroundColor);
+    nvgFill(vg);
+
+    // text
+    std::shared_ptr<Font> font = APP->window->loadFont(asset::plugin(pluginInstance, "res/Pokemon.ttf"));
+    if (font) {
+    nvgFontSize(vg, 15);
+    nvgFontFaceId(vg, font->handle);
+    nvgTextLetterSpacing(vg, 2);
+    }
+
+    std::stringstream to_display;
+    if (module) {
+        to_display << std::setw(3) << module->display_data[val_type];
+    } else {
+        to_display << std::setw(3) << "C#";
+    }
+
+    Vec textPos = Vec(2.0f, 17.0f);
+    NVGcolor textColor = nvgRGB(0x00, 0x00, 0x00);
+    nvgFillColor(vg, textColor);
+    nvgText(vg, textPos.x, textPos.y, to_display.str().c_str(), NULL);
+  }
+};
+
+struct uQTrimpotSnap : RoundKnob {
+	uQTrimpotSnap() {
+		snap = true;
+		setSvg(APP->window->loadSvg(asset::plugin(pluginInstance,"res/ComponentLibrary/AHTrimpot.svg")));
+	}
+};
 
 struct uQuantWidget : ModuleWidget {
     uQuantWidget(uQuant *module);
@@ -197,7 +209,6 @@ struct uQuantWidget : ModuleWidget {
 uQuantWidget::uQuantWidget(uQuant *module) {
 		setModule(module);
 
-    UI ui;
     box.size = Vec(30, 380);
 
     {
@@ -212,32 +223,32 @@ uQuantWidget::uQuantWidget(uQuant *module) {
 
     addInput(createInput<PJ301MPort>(Vec(leftPad, 41), module, uQuant::IN_INPUT));
 
-    if(module != NULL){
-        uQuantDisplayWidget *displayKey = new uQuantDisplayWidget();
-        displayKey = new uQuantDisplayWidget();
-        displayKey->box.pos = Vec(leftPad, 71);
-        displayKey->box.size = Vec(25, 25);
-        displayKey->value = &module->keyValue[0];
-        addChild(displayKey);
-        addParam(createParam<AHTrimpotSnap>(Vec(knobLeftPad, 101), module, uQuant::KEY_PARAM)); // 12 notes
-        addInput(createInput<PJ301MPort>(Vec(leftPad, 125), module, uQuant::KEY_INPUT));
+    uQuantDisplayWidget *displayKey = new uQuantDisplayWidget();
+    displayKey = new uQuantDisplayWidget();
+    displayKey->box.pos = Vec(leftPad, 71);
+    displayKey->box.size = Vec(25, 25);
+    displayKey->module = module;
+    displayKey->val_type = "key_value";
+    addChild(displayKey);
+    addParam(createParam<uQTrimpotSnap>(Vec(knobLeftPad, 101), module, uQuant::KEY_PARAM));  // 12 notes
+    addInput(createInput<PJ301MPort>(Vec(leftPad, 125), module, uQuant::KEY_INPUT));
 
-        uQuantDisplayWidget *displayScale = new uQuantDisplayWidget();
-        displayScale = new uQuantDisplayWidget();
-        displayScale->box.pos = Vec(leftPad, 155);
-        displayScale->box.size = Vec(25, 25);
-        displayScale->value = &module->scaleValue[0];
-        addChild(displayScale);
-    }
+    uQuantDisplayWidget *displayScale = new uQuantDisplayWidget();
+    displayScale = new uQuantDisplayWidget();
+    displayScale->box.pos = Vec(leftPad, 155);
+    displayScale->box.size = Vec(25, 25);
+    displayScale->module = module;
+    displayScale->val_type = "scale_value";
+    addChild(displayScale);
 
-    addParam(createParam<AHTrimpotSnap>(Vec(knobLeftPad, 185), module, uQuant::SCALE_PARAM)); // 12 notes
+    addParam(createParam<uQTrimpotSnap>(Vec(knobLeftPad, 185), module, uQuant::SCALE_PARAM)); // 12 notes
     addInput(createInput<PJ301MPort>(Vec(leftPad, 209), module, uQuant::SCALE_INPUT));
 
     // Octave
-    addParam(createParam<AHTrimpotSnap>(Vec(knobLeftPad, 240), module, uQuant::SHIFT_PARAM));
+    addParam(createParam<uQTrimpotSnap>(Vec(knobLeftPad, 240), module, uQuant::SHIFT_PARAM));
 
     // Transpose
-    addParam(createParam<AHTrimpotSnap>(Vec(knobLeftPad,265), module, uQuant::TRANS_PARAM)); // 12 notes
+    addParam(createParam<uQTrimpotSnap>(Vec(knobLeftPad,265), module, uQuant::TRANS_PARAM)); // 12 notes
     addInput(createInput<PJ301MPort>(Vec(leftPad, 290), module, uQuant::TRANS_INPUT));
 
     // Outputs
